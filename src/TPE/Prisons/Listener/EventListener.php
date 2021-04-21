018fb769c76912bcd75dc0c1350d9ef415c27fe5
TPE <60147636+TPEimperialPE@users.noreply.github.com>
2021-04-21 19:28:02 +0100

    join and chat stuff

--- src/TPE/Prisons/Listener/EventListener.php
+++ src/TPE/Prisons/Listener/EventListener.php
@@ -11,39 +11,19 @@ use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\utils\TextFormat;
 use TPE\Prisons\Data\BaseDB;
 use TPE\Prisons\Prisons;
-use pocketmine\event\player\PlayerPreLoginEvent;
+use pocketmine\event\player\PlayerJoinEvent;
 use TPE\Prisons\Utils;
 
 final class EventListener implements Listener {
 
     /**
-     * @param PlayerPreLoginEvent $event
+     * @param PlayerJoinEvent $event
      * @return void
      */
-    public function onPlayerLogin(PlayerPreLoginEvent $event) : void {
-        Prisons::get()->getPrisonRank($event->getPlayer(), function(array $rows) use($event) {
-            $dr = [];
-            foreach($rows as $row) {
-                if(isset($row['prisonrank'])) {
-                    $dr[0] = $row['prisonrank'];
-                } else {
-                    $dr[0] = "a";
-                }
-            }
-            Prisons::get()->getPrisonPrestige($event->getPlayer(), function(array $rows) use($event, $dr) {
-                $dp = [];
-                foreach($rows as $row) {
-                    if(isset($row['prestige'])) {
-                        $dp[0] = $row['prestige'];
-                    } else {
-                        $dp[0] = 0;
-                    }
-                }
-                Prisons::get()->getDatabaseConnector()->executeInsert(
-                BaseDB::PRISONS_REGISTER_PLAYER,
-                ['username' => $event->getPlayer()->getLowerCaseName(), 'prisonrank' => $dr[0], 'prestige' => $dp[0]]);
-            });
-        });
+    public function onPlayerLogin(PlayerJoinEvent $event) : void {
+        $player = $event->getPlayer();
+        if(($p = Prisons::get()->getPlayerManager()->getPlayer($player)) === null) $member = Prisons::get()->getPlayerManager()->createPlayer($player);
+        if($member->getUsername() !== $player->getName()) $member->setUsername($player->getName());
     }
 
     /**
@@ -86,25 +66,8 @@ final class EventListener implements Listener {
      * @return void
      */
     public function onChat(PlayerChatEvent $event): void {
-        $event->setCancelled();
-        if(Prisons::get()->getPermissionManager() === "pureperms") {
-            Prisons::get()->getPrisonRank($event->getPlayer(), function (array $rows) use ($event) {
-                $currentRank = "";
-                foreach ($rows as $row) {
-                    $currentRank = $row['prisonrank'];
-                }
-
-                Prisons::get()->getPrisonPrestige($event->getPlayer(), function (array $rows) use ($event, $currentRank) {
-                    $currentPrestige = 0;
-                    foreach ($rows as $row) {
-                        $currentPrestige = $row['prestige'];
-                    }
-
-                    $format = str_replace(["{PRISON_RANK}", "{PRISON_PRESTIGE}"], [Utils::getRankName($currentRank), $currentPrestige], $event->getFormat());
-                    // Am looking for some assistance on this issue as it is not ideal. 
-                    Prisons::get()->getServer()->broadcastMessage($format);
-                });
-            });
-        }
+        $member = Prisons::get()->getPlayerManager()->getPlayer($event->getPlayer());
+        $format = str_replace(["PRISON_RANK", "PRISON_PRESTIGE"], [$member->getPrisonRank(), $member->getPrestige()], $event->getFormat());
+        $event->setFormat($format);
     }
 }
