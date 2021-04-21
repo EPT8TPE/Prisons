1807ab15ab8f55aa4991e99605f47cbc1817ac79
TPE <60147636+TPEimperialPE@users.noreply.github.com>
2021-04-21 08:33:43 +0100

    Update mysql.sql

--- resources/mysql.sql
+++ resources/mysql.sql
@@ -2,69 +2,36 @@
 -- #{ prisons
 -- #  { init
 CREATE TABLE IF NOT EXISTS prisons_players (
-    username VARCHAR(32) NOT NULL,
-    prisonrank VARCHAR(1) NOT NULL,
-    prestige INT UNSIGNED NOT NULL,
-    PRIMARY KEY(username),
-    UNIQUE(username)
+    uuid VARCHAR(36) PRIMARY KEY,
+    username VARCHAR(16),
+    prisonrank STRING DEFAULT 'a',
+    prestige INT DEFAULT 0
 );
 -- #  }
 
 -- #  { initplayer
--- #    :username string
-SELECT
-  prisonrank,
-  prestige
-FROM prisons_players WHERE username=:username;
+SELECT *
+FROM prisons_players;
 -- #  }
 
--- #  { player
--- #    { register
+-- #  { create
+-- #      :uuid string
 -- #      :username string
-INSERT INTO prisons_players(
-  username,
-  prisonrank,
-  prestige
-) VALUES (
-  :username,
-  'a',
-  0
-) ON DUPLICATE KEY UPDATE
-prisonrank=VALUES(prisonrank),
-prestige=VALUES(prestige);
+-- #      :prisonrank string
+-- #      :prestige int
+INSERT INTO prisons_players(uuid, username, prisonrank , prestige)
+VALUES (:uuid, :username, :prisonrank, :prestige);
 -- #    }
 
--- #    { update
--- #      { prestige
--- #        :username string
--- #        :prestige int
-UPDATE prisons_players SET
+-- #  { update
+-- #      :uuid string
+-- #      :username string
+-- #      :prisonrank string
+-- #      :prestige int
+UPDATE prison_players 
+SET username=:username,
+    prisonrank=:prisonrank,
     prestige=:prestige
-WHERE username=:username;
--- #      }
-
--- #      { prisonrank
--- #        :username string
--- #        :prisonrank string
-UPDATE prisons_players SET
-    prisonrank=:prisonrank
-WHERE username=:username;
--- #      }
--- #    }
+WHERE uuid = :uuid;
 -- #  }
-
--- #  { get
--- #    { player
--- #      { prestige
--- #        :username string
-SELECT prestige FROM prisons_players WHERE username=:username;
--- #      }
-
--- #      { prisonrank
--- #        :username string
-SELECT prisonrank FROM prisons_players WHERE username=:username;
--- #      }
--- #    }
--- #  }
-
--- #}
+-- # }
