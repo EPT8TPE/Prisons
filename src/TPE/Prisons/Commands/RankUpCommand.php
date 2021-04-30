c1f73825014898e7309cfa5b6d46a10de466dfdd
TPE <60147636+TPEimperialPE@users.noreply.github.com>
2021-04-30 08:32:44 +0100

    fix

--- src/TPE/Prisons/Commands/RankUpCommand.php
+++ src/TPE/Prisons/Commands/RankUpCommand.php
@@ -14,6 +14,7 @@ use TPE\Prisons\Listener\PrisonListener\PrisonRankUpEvent;
 use TPE\Prisons\Prisons;
 use TPE\Prisons\Utils;
 use _64FF00\PurePerms\PurePerms;
+use pocketmine\command\ConsoleCommandSender;
 
 final class RankUpCommand extends Command implements PluginIdentifiableCommand {
 
@@ -66,7 +67,9 @@ final class RankUpCommand extends Command implements PluginIdentifiableCommand {
         $price = Utils::getRankUpPrice($currentRank, $currentPrestige);
 
         if(Utils::processTransaction($sender, $price)) {
-            $event = (new PrisonRankUpEvent($sender, (string)$nextRank, $currentRank, Utils::getRankCommands($currentRank), Utils::getRankPermissions($currentRank, "added"), Utils::getRankPermissions($currentRank, "removed")))->call();
+            $event = (new PrisonRankUpEvent($sender, (string)$nextRank, $currentRank, Utils::getRankCommands($currentRank), Utils::getRankPermissions($currentRank, "added"), Utils::getRankPermissions($currentRank, "removed")));
+            $event->call();
+            if($event->isCancelled()) return;
 
             $member = Prisons::get()->getPlayerManager()->getPlayer($event->getPlayer());
             $member->setPrisonRank($nextRank);
